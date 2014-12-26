11ea577e504ed5831f3edec4bd5b08c0f5a3352f
incredible.cha <incredible.cha@gmail.com>
2014-12-26 05:21:26 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebBetween::Application.config.secret_key_base = '18a63ed18062d9e6ccf5bc13ac9100a05d7377bdd2ba87d2a39a8fe9d5344a5d21b6c6f221cd8b1193f7628008409f5cb5e7a2f95afde6f0e38d3343a247f6f4'
